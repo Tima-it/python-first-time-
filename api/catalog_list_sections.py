7aa748c6d35e18de4e07d7cb3723560a2ce1aa1b
Tima <nebagatov@yandex.ru>
2023-04-10 19:19:55 +0500

    style: format code

--- api/catalog_list_sections.py
+++ api/catalog_list_sections.py
@@ -1,22 +1,20 @@
 from http.server import BaseHTTPRequestHandler
 import sqlite3 as sq
 
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         # self.wfile.write('Hello, world!'.encode('utf-8'))
 
         con = sq.connect("db")
         cur = con.cursor()
 
-
         con.commit()
         sections = cur.execute("SELECT * FROM `catalog_sections`")
-        self.wfile.write(print(*sections, sep = ", "))
+        self.wfile.write(print(*sections, sep=", "))
 
         return
-
-
